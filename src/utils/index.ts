1e73add3c4c9958a515a4b9f3c0b890fe99df4d8
yuche <i@yuche.me>
2021-04-22 09:48:23 +0800

    fix: parse address

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,14 @@
+import { parseAddress } from '@nervosnetwork/ckb-sdk-utils'
+
 export const sleep = async (ms: number): Promise<void> =>
   await new Promise((resolve) => setTimeout(resolve, ms))
 
 export const isValidCkbLongAddress = (address: string): boolean => {
+  try {
+    parseAddress(address)
+  } catch {
+    return false
+  }
   return (
     address.length === 95 &&
     (address.startsWith('ckb') || address.startsWith('ckt')) &&
