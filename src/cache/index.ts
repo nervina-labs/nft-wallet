4a21efbdc4460741a0f457b99b2e039edaa962de
yuche <i@yuche.me>
2021-05-12 18:02:33 +0800

    refactor: login page

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,24 +1,32 @@
-const UNIPASS_ADDRESS_KEY = 'unipass_address_key'
-const UNIPASS_EMAIL_KEY = 'unipass_email_key'
-const UNIPASS_LOGIN_DATE = 'unipass_login_date'
+// const UNIPASS_ADDRESS_KEY = 'unipass_address_key'
+// const UNIPASS_EMAIL_KEY = 'unipass_email_key'
+// const UNIPASS_LOGIN_DATE = 'unipass_login_date'
 
-export const unipassCache = {
-  getUnipassAddress(): string | null {
-    return localStorage.getItem(UNIPASS_ADDRESS_KEY)
+const I18N_LANG = 'i18nextLng'
+
+export const LocalCache = {
+  // getUnipassAddress(): string | null {
+  //   return localStorage.getItem(UNIPASS_ADDRESS_KEY)
+  // },
+  // setUnipassAddress(address: string): void {
+  //   localStorage.setItem(UNIPASS_ADDRESS_KEY, address)
+  // },
+  // getUnipassEmail(): string | null {
+  //   return localStorage.getItem(UNIPASS_EMAIL_KEY)
+  // },
+  // setUnipassEmail(email: string): void {
+  //   localStorage.setItem(UNIPASS_EMAIL_KEY, email)
+  // },
+  // getUnipassLoginDate(): string | null {
+  //   return localStorage.getItem(UNIPASS_LOGIN_DATE)
+  // },
+  // setUnipassLoginDate(date: string) {
+  //   localStorage.setItem(UNIPASS_LOGIN_DATE, date)
+  // },
+  setI18nLng(lang: 'zh' | 'en') {
+    localStorage.setItem(I18N_LANG, lang)
   },
-  setUnipassAddress(address: string): void {
-    localStorage.setItem(UNIPASS_ADDRESS_KEY, address)
-  },
-  getUnipassEmail(): string | null {
-    return localStorage.getItem(UNIPASS_EMAIL_KEY)
-  },
-  setUnipassEmail(email: string): void {
-    localStorage.setItem(UNIPASS_EMAIL_KEY, email)
-  },
-  getUnipassLoginDate(): string | null {
-    return localStorage.getItem(UNIPASS_LOGIN_DATE)
-  },
-  setUnipassLoginDate(date: string) {
-    localStorage.setItem(UNIPASS_LOGIN_DATE, date)
+  getI18nLng() {
+    return localStorage.getItem(I18N_LANG) ?? 'zh'
   },
 }
