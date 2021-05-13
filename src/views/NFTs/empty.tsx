aacec4c7cb023cc57d8a8bdcdd089ee6ca38bafe
yuche <i@yuche.me>
2021-05-13 16:13:49 +0800

    refactor: nft empty

--- src/views/NFTs/empty.tsx
+++ src/views/NFTs/empty.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
-import { ReactComponent as EmptySvg } from '../../assets/svg/empty.svg'
+import Emptypng from '../../assets/img/empty.png'
 import { NFT_EXPLORER_URL } from '../../constants'
+import { LazyLoadImage } from '../../components/Image'
 
 const Container = styled.div`
   margin-top: 80px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  img {
+    margin-top: 100px;
+  }
   .desc {
-    margin-top: 12px;
+    margin-top: 30px;
     font-size: 16px;
     line-height: 22px;
     color: rgba(0, 0, 0, 0.6);
     font-weight: 600;
   }
   .link {
-    margin-top: 32px;
+    margin-top: 15px;
     font-weight: 600;
     font-size: 14px;
     line-height: 20px;
@@ -30,7 +34,7 @@ export const Empty: React.FC = () => {
   const { t } = useTranslation('translations')
   return (
     <Container>
-      <EmptySvg />
+      <LazyLoadImage src={Emptypng} width={260} height={172} />
       <div className="desc">{t('nfts.no-data')}</div>
       <a
         className="link"
