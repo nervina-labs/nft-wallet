88d8423bdd41055988044808917f785427395712
yuche <i@yuche.me>
2021-05-14 16:16:45 +0800

    refactor: t.unipass.me env

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -8,7 +8,7 @@ export const NODE_URL =
 export const INDEXER_URL =
   process.env.REACT_APP_INDEXER_URL ?? 'https://testnet.ckb.dev/indexer'
 export const UNIPASS_URL =
-  process.env.REACT_APP_UNIPASS_URL ?? 'https://unipass.me'
+  process.env.REACT_APP_UNIPASS_URL ?? 'https://t.unipass.me'
 export const NFT_EXPLORER_URL =
   process.env.REACT_APP_NFT_EXPLORER_URL ??
   'https://ckb-nft-explorer.staging.nervina.cn'
