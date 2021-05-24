9e1aa87fdae8364b3475f1c121966bc0063951ec
yuche <i@yuche.me>
2021-05-24 10:24:50 +0800

    refactor: move style to different file

--- src/views/Transfer/index.tsx
+++ src/views/Transfer/index.tsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { Redirect, useHistory, useLocation, useParams } from 'react-router'
-import styled from 'styled-components'
 import { Appbar } from '../../components/Appbar'
 import { NFTDetail, Query } from '../../models'
 import { RoutePath } from '../../routes'
@@ -26,160 +25,11 @@ import { useWidth } from '../../hooks/useWidth'
 import { Limited } from '../../components/Limited'
 import { Creator } from '../../components/Creator'
 import { useQuery } from 'react-query'
-import { MainContainer } from '../../styles'
 import { CONTAINER_MAX_WIDTH, IS_IPHONE, IS_MAINNET } from '../../constants'
 import UnipassProvider from '../../pw/UnipassProvider'
 import { Address, AddressType } from '@lay2/pw-core'
 import { useTranslation } from 'react-i18next'
-
-const Container = styled(MainContainer)`
-  display: flex;
-  flex-direction: column;
-  .content {
-    display: flex;
-    flex-direction: column;
-    margin: 0 36px;
-    height: 100%;
-    flex: 1;
-    label {
-      font-weight: 600;
-      font-size: 14px;
-      line-height: 16px;
-      color: rgba(0, 0, 0, 0.6);
-      margin-top: 120px;
-      margin-bottom: 12px;
-    }
-    .alert {
-      font-weight: 600;
-      font-size: 12px;
-      line-height: 17px;
-      display: flex;
-      align-items: center;
-      margin-top: 10px;
-      height: 34px;
-      svg {
-        width: 12px;
-        height: 12px;
-        margin-right: 4px;
-      }
-      &.error {
-        color: #d03a3a;
-      }
-      &.info {
-        color: #2196f3;
-      }
-    }
-    .form {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      .input {
-        background: transparent;
-        width: 100%;
-        border: none;
-        overflow: auto;
-        outline: none;
-        border-radius: 0;
-        -webkit-box-shadow: none;
-        -moz-box-shadow: none;
-        box-shadow: none;
-        resize: none;
-        border-bottom: 1px solid #000000;
-      }
-      svg {
-        margin-left: 10px;
-      }
-    }
-    .action {
-      display: flex;
-      justify-content: center;
-      margin-top: 35px;
-    }
-    .desc {
-      margin-top: auto;
-      margin-bottom: 50px;
-      font-weight: 600;
-      font-size: 14px;
-      line-height: 20px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      text-align: center;
-      color: rgba(0, 0, 0, 0.6);
-      p {
-        margin: 0;
-      }
-    }
-  }
-`
-
-const DrawerContainer = styled.div`
-  margin: 32px 36px;
-  margin-bottom: 0;
-  .header {
-    display: flex;
-    align-items: center;
-    label {
-      font-style: normal;
-      font-weight: 600;
-      font-size: 14px;
-      line-height: 16px;
-      color: rgba(0, 0, 0, 0.6);
-      margin: 0;
-      display: flex;
-      align-items: center;
-    }
-    svg {
-      margin-left: auto;
-      cursor: pointer;
-    }
-  }
-  .card {
-    margin-top: 8px;
-    margin-bottom: 32px;
-    display: flex;
-    height: 80px;
-    .info {
-      margin: 4px 0;
-      flex: 1;
-      margin-left: 12px;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      font-weight: 600;
-      font-size: 12px;
-      line-height: 17px;
-      overflow: hidden;
-      .title {
-        color: #000000;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        overflow: hidden;
-      }
-      .row {
-        color: rgba(0, 0, 0, 0.6);
-      }
-    }
-  }
-  .address {
-    margin-top: 12px;
-    font-size: 14px;
-    line-height: 16px;
-    color: #000000;
-    font-weight: 600;
-    word-break: break-all;
-  }
-  .center {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 32px;
-    margin-bottom: 40px;
-    .loading {
-      margin-left: 10px;
-    }
-  }
-`
+import { Container, DrawerContainer } from './styled'
 
 export enum FailedMessage {
   SignFail = 'sign-fail',
