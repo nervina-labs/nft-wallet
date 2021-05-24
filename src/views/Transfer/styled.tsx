5625b8f4efa0f7f7e85f5ceb964c5ed40de14b72
yuche <i@yuche.me>
2021-05-24 23:02:27 +0800

    refactor: transfer page

--- src/views/Transfer/styled.tsx
+++ src/views/Transfer/styled.tsx
@@ -1,103 +1,54 @@
 import styled from 'styled-components'
+import Bg from '../../assets/img/transfer-bg.png'
 import { MainContainer } from '../../styles'
 
 export const Container = styled(MainContainer)`
   display: flex;
   flex-direction: column;
   background: #ecf2f5;
-  .content {
-    display: flex;
-    flex-direction: column;
-    margin: 0 36px;
-    height: 100%;
-    flex: 1;
-    label {
-      font-weight: 600;
-      font-size: 14px;
-      line-height: 16px;
-      color: rgba(0, 0, 0, 0.6);
-      margin-top: 120px;
-      margin-bottom: 12px;
+  > header {
+    background: transparent;
+    box-shadow: none;
+  }
+  section {
+    &.main {
+      flex: 1;
+      background: #ecf2f5 url(${Bg});
+      background-repeat: no-repeat;
+      background-size: contain;
+      background-position: bottom;
     }
-    .alert {
-      font-weight: 600;
-      font-size: 12px;
-      line-height: 17px;
-      display: flex;
-      align-items: center;
-      margin-top: 10px;
-      height: 34px;
-      svg {
-        width: 12px;
-        height: 12px;
-        margin-right: 4px;
-      }
-      &.error {
-        color: #d03a3a;
-      }
-      &.info {
-        color: #2196f3;
-      }
-    }
-    .form {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      .input {
-        background: transparent;
-        width: 100%;
-        border: none;
-        overflow: auto;
-        outline: none;
-        border-radius: 0;
-        -webkit-box-shadow: none;
-        -moz-box-shadow: none;
-        box-shadow: none;
-        resize: none;
-        border-bottom: 1px solid #000000;
-      }
-      svg {
-        margin-left: 10px;
-      }
-    }
-    .action {
-      display: flex;
-      justify-content: center;
-      margin-top: 35px;
+    .boxes {
+      height: 340px;
+      max-height: 340px;
+      overflow: hidden;
     }
     .desc {
-      margin-top: auto;
-      margin-bottom: 50px;
-      font-weight: 600;
-      font-size: 14px;
-      line-height: 20px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
+      font-size: 12px;
+      color: #8d8989;
+      line-height: 16px;
+      margin-top: 10px;
       text-align: center;
-      color: rgba(0, 0, 0, 0.6);
-      p {
-        margin: 0;
-      }
     }
+  }
+  .success {
+    color: #1fd345;
+    font-size: 12px;
   }
 `
 
 export const DrawerContainer = styled.div`
-  margin: 32px 36px;
+  margin: 15px;
   margin-bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   .header {
     display: flex;
-    align-items: center;
-    label {
-      font-style: normal;
-      font-weight: 600;
-      font-size: 14px;
-      line-height: 16px;
-      color: rgba(0, 0, 0, 0.6);
-      margin: 0;
-      display: flex;
-      align-items: center;
+    width: 100%;
+    span {
+      flex: 1;
     }
     svg {
       margin-left: auto;
@@ -105,48 +56,129 @@ export const DrawerContainer = styled.div`
     }
   }
   .card {
-    margin-top: 8px;
-    margin-bottom: 32px;
-    display: flex;
-    height: 80px;
-    .info {
-      margin: 4px 0;
-      flex: 1;
-      margin-left: 12px;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      font-weight: 600;
-      font-size: 12px;
-      line-height: 17px;
-      overflow: hidden;
-      .title {
-        color: #000000;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        overflow: hidden;
-      }
-      .row {
-        color: rgba(0, 0, 0, 0.6);
-      }
-    }
+    margin-top: 20px;
+  }
+  .title {
+    margin-top: 20px;
+    font-size: 12px;
+    font-weight: bold;
+    color: #0e0e0e;
   }
   .address {
-    margin-top: 12px;
-    font-size: 14px;
-    line-height: 16px;
-    color: #000000;
-    font-weight: 600;
+    margin-top: 10px;
+    font-size: 12px;
+    color: #0e0e0e;
     word-break: break-all;
+    width: 250px;
+    text-align: center;
+    margin-bottom: 0;
   }
   .center {
     display: flex;
     align-items: center;
     justify-content: center;
     margin-top: 32px;
-    margin-bottom: 40px;
+    margin-bottom: 32px;
     .loading {
       margin-left: 10px;
+    }
+  }
+`
+
+export const Box = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 220px;
+  position: relative;
+  background: white;
+  border-radius: 20px;
+  margin: 0 15px;
+  margin-top: 80px;
+  padding: 0 40px;
+  z-index: 3;
+  label {
+    font-size: 16px;
+    color: #0e0e0e;
+    font-weight: bold;
+    margin: 20px 0;
+  }
+  .form {
+    border-radius: 30px;
+    background: #f0f0f1;
+    min-height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 10px 20px;
+    .input {
+      background: transparent;
+      flex: 1;
+      width: 100%;
+      border: none;
+      overflow: auto;
+      outline: none;
+      border-radius: 0;
+      -webkit-box-shadow: none;
+      -moz-box-shadow: none;
+      box-shadow: none;
+      resize: none;
+      margin-right: 4px;
+    }
+    svg {
+      path {
+        fill: #6e8ae6;
+      }
+    }
+  }
+
+  .action {
+    position: relative;
+    z-index: 3;
+    top: 30px;
+    .transfer {
+      width: 66px;
+      height: 66px;
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      background: #fd5c31;
+      box-shadow: 11px 6px 9px 0px rgba(253, 92, 49, 0.19);
+      font-size: 20px;
+      border-radius: 50%;
+      color: white;
+      &.disabled {
+        background-color: #ddd;
+        color: #898989;
+        cursor: not-allowed;
+        box-shadow: none;
+      }
+      img {
+        width: 22px;
+      }
+    }
+  }
+
+  .alert {
+    font-weight: 600;
+    font-size: 10px;
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+    height: 34px;
+    svg {
+      width: 12px;
+      height: 12px;
+      margin-right: 4px;
+    }
+    &.error {
+      color: #d03a3a;
+    }
+    &.info {
+      color: #2196f3;
     }
   }
 `
