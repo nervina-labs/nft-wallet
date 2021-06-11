2a036f22feb0ab0b76f4fef5c0c12d865205a96b
yuche <i@yuche.me>
2021-06-11 14:32:23 +0800

    feat:  remove webpack config

--- craco.config.js
+++ craco.config.js
@@ -2,18 +2,4 @@ module.exports = {
   babel: {
     plugins: ['babel-plugin-styled-components'],
   },
-  webpack: {
-    configure: (webpackConfig) => {
-      if (process.env.NODE_ENV === 'production') {
-        const TerserPlugin = webpackConfig.optimization.minimizer.find(
-          (i) => i.constructor.name === 'TerserPlugin'
-        )
-        if (TerserPlugin) {
-          TerserPlugin.options.terserOptions.compress.drop_console = true
-        }
-      }
-
-      return webpackConfig
-    },
-  },
 }
