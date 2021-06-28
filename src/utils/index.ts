152943d499f0face1cf2b7a6c680c07c86584a95
yuche <i@yuche.me>
2021-06-28 17:37:39 +0800

    feat: scroll restoration for explore and home page

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -72,3 +72,13 @@ export function getRandomNumber(min: number, max: number): number {
 export function getRandomBool(): boolean {
   return Math.random() > 0.5
 }
+
+export function throttle(fn: () => void, wait: number): () => void {
+  let time = Date.now()
+  return function () {
+    if (time + wait - Date.now() < 0) {
+      fn()
+      time = Date.now()
+    }
+  }
+}
