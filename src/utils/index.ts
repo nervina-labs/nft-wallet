ab4e520d7e6ae88ca2a456efbcb25698f52320b1
yuche <i@yuche.me>
2021-07-08 20:09:23 +0800

    refactor: allow transfer to all valid ckb address

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,14 +5,13 @@ import { INFURA_ID } from '../constants'
 export const sleep = async (ms: number): Promise<void> =>
   await new Promise((resolve) => setTimeout(resolve, ms))
 
-export const verifyCkbLongAddress = (address: string): boolean => {
+export const verifyCkbAddress = (address: string): boolean => {
   try {
     parseAddress(address)
   } catch {
     return false
   }
   return (
-    address.length === 95 &&
     (address.startsWith('ckb') || address.startsWith('ckt')) &&
     /^[A-Za-z0-9]+$/.test(address)
   )
