4832723218c1089a83ed450ab501c9746a5fcf47
Yu Che <i@yuche.me>
2021-07-14 09:36:00 +0800

    Update src/data/help.ts
    Co-authored-by: ybian <chuijiaolianying@gmail.com>

--- src/data/help.ts
+++ src/data/help.ts
@@ -3,7 +3,7 @@ export const HELP_UNIPASS_CN =
   'https://www.yuque.com/faqbangzhuzhichi/gsg82h/qa0dmm'
 export const HELP_CENTER_EN = 'https://www.yuque.com/faqbangzhuzhichi/zizddp?language=en-us'
 export const HELP_UNIPASS_EN =
-  'https://www.yuque.com/faqbangzhuzhichi/zizddp/stspbp'
+  'https://www.yuque.com/faqbangzhuzhichi/zizddp/stspbp?language=en-us'
 
 export const getHelpCenterUrl = (lang: string): string =>
   lang === 'en' ? HELP_CENTER_EN : HELP_CENTER_CN
