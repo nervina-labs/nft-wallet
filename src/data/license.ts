7a03f7c12c5f16866cff18d911d635d9ab306e71
yuche <i@yuche.me>
2021-07-27 17:31:47 +0800

    refactor: update url

--- src/data/license.ts
+++ src/data/license.ts
@@ -1,7 +1,7 @@
 export const LICENSE_URL_ZH =
-  'https://www.yuque.com/faqbangzhuzhichi/zizddp/stspbp'
+  'https://www.yuque.com/zhouyun-llkz5/nervina/op93nl'
 export const LICENSE_URL_EN =
-  'https://www.yuque.com/faqbangzhuzhichi/zizddp/stspbp?language=en-us'
+  'https://www.yuque.com/zhouyun-llkz5/cmszgn/mi2r9z?language=en-us'
 
 export const getLicenseUrl = (lang: string): string =>
   lang !== 'en' ? LICENSE_URL_ZH : LICENSE_URL_EN
