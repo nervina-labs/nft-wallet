df5f7cc6f717dfaac8d9922fe94ec84df54f711b
yuche <i@yuche.me>
2021-09-14 15:52:34 +0800

    chore: remove some files

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -62,10 +62,9 @@ export const TICKET_APP_URL = IS_MAINNET
   ? 'https://ticket.unipass.xyz'
   : 'https://t.ticket.unipass.xyz'
 
-export const WECHAT_APP_ID =
-  process.env.REACT_APP_WECHAT_APP_ID ?? 'wx32f5170ce791de49'
+export const WECHAT_APP_ID = process.env.REACT_APP_WECHAT_APP_ID
 
-export const WEAPP_ID = process.env.REACT_APP_WEAPP_ID ?? 'gh_61a4f3f229f5'
+export const WEAPP_ID = process.env.REACT_APP_WEAPP_ID
 export const DAS_INDEXER_URL =
   process.env.REACT_APP_DAS_INDEXER_URL ?? 'https://das.nervina.cn'
 
