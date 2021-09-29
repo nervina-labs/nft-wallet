1f4df565df7ff4907ec0386ac6bba235c631d804
yuche <i@yuche.me>
2021-09-29 15:01:39 +0800

    refactor: adjust expire time

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -105,7 +105,7 @@ export function useSetAccount() {
           : {
               ...prevAccount,
               ...account,
-              expireTime: dayjs().add(1, 'day').toISOString(),
+              expireTime: dayjs().add(7, 'day').toISOString(),
             }
       })
     },
