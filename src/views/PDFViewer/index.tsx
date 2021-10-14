4edbdc9b8719b96a9861ef88574f242f30898ac3
yuche <i@yuche.me>
2021-10-14 15:45:02 +0800

    feat: custon pdf viewer

--- src/views/PDFViewer/index.tsx
+++ src/views/PDFViewer/index.tsx
@@ -1,6 +1,44 @@
 import React, { useState } from 'react'
-import { Document, Page, pdfjs } from 'react-pdf'
+import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.webpack'
+import styled from 'styled-components'
 import { useRouteQuery } from '../../hooks/useRouteQuery'
+
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+}
+
+const Container = styled.div`
+  overflow-x: hidden;
+  .pdf__container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .pdf__container__load {
+    color: white;
+  }
+  .pdf__container__document .react-pdf__Document {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .pdf__container__document .react-pdf__Page {
+    max-width: calc(100% - 2em);
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
+    margin: 1em;
+  }
+  .pdf__container__document .react-pdf__Page canvas {
+    max-width: 100%;
+    height: auto !important;
+  }
+  .pdf__container__document .react-pdf__message {
+    padding: 20px;
+    color: white;
+  }
+`
 
 export function PDFViewer() {
   pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js'
@@ -12,16 +50,22 @@ export function PDFViewer() {
   }
 
   return (
-    <div className="main" style={{ display: 'flex', justifyContent: 'center' }}>
-      <Document
-        file={decodeURIComponent(url)}
-        onLoadSuccess={onDocumentLoadSuccess}
-        loading=""
-      >
-        {Array.from(new Array(numPages), (el, index) => (
-          <Page key={`page_${index + 1}`} pageNumber={index + 1} loading="" />
-        ))}
-      </Document>
-    </div>
+    <Container>
+      <div className="pdf">
+        <div className="pdf__container">
+          <div className="pdf__container__document">
+            <Document
+              file={decodeURIComponent(url)}
+              onLoadSuccess={onDocumentLoadSuccess}
+              options={options}
+            >
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+              ))}
+            </Document>
+          </div>
+        </div>
+      </div>
+    </Container>
   )
 }
