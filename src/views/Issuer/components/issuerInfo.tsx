ae122a3d3ee87f6043bf9fcadac10241cc05675e
INS <j893412899@outlook.com>
2021-10-20 15:35:12 +0800

    to: Restructure issuer

---
+++ src/views/Issuer/components/issuerInfo.tsx
@@ -0,0 +1,19 @@
+import { Flex, Avatar } from '@mibao-ui/components'
+import React from 'react'
+import { useIssuerInfo } from '../hooks/useIssuerInfo'
+import { useParams } from 'react-router'
+
+export const IssuerInfo: React.FC = () => {
+  const { id } = useParams<{ id: string }>()
+  const { data, isLoading, refetch, error } = useIssuerInfo(id)
+
+  return (
+    <Flex>
+      <Avatar
+        src={data?.avatar_url}
+        isVerified={data?.verified_info?.is_verified}
+        isBanned={data?.is_issuer_banned}
+      />
+    </Flex>
+  )
+}
