6baa1f7b8cbe0defaba320ee14b9e94b847814ff
INS <j893412899@outlook.com>
2021-10-26 17:13:16 +0800

    feat: Add on sale list

--- src/components/InfiniteListNext/index.tsx
+++ src/components/InfiniteListNext/index.tsx
@@ -150,8 +150,9 @@ export function InfiniteListNext<
           scrollThreshold={scrollThreshold}
           loader={loader ?? <Loading />}
           endMessage={<Tips>{dataLength <= 5 ? ' ' : noMoreElement}</Tips>}
+          style={{ overflow: 'visible' }}
         >
-          <Grid templateColumns="repeat(2, 1fr)" gap="10px">
+          <Grid templateColumns={`repeat(${columnCount}, 1fr)`} gap="10px">
             {columns.map((column) => (
               <Virtuoso
                 useWindowScroll
--- src/views/Issuer/components/nftCards.tsx
+++ src/views/Issuer/components/nftCards.tsx
@@ -7,6 +7,8 @@ import {
   TabPanels,
   Image,
   AspectRatio,
+  NftImage,
+  Flex,
 } from '@mibao-ui/components'
 import React, { useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
@@ -17,6 +19,16 @@ import { Empty } from '../../NFTs/empty'
 import { InfiniteListNext } from '../../../components/InfiniteListNext'
 import { useAPI } from '../../../hooks/useAccount'
 import { useParams } from 'react-router'
+import { Like } from '../../../components/Like'
+import styled from 'styled-components'
+
+const LineClamp = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+`
 
 export const NftCards: React.FC = () => {
   const { id } = useParams<{ id: string }>()
@@ -27,12 +39,13 @@ export const NftCards: React.FC = () => {
   const { replace, location } = useHistory()
   const api = useAPI()
   const [t] = useTranslation('translations')
-  const [defaultIndex] = useState(
+  const [index, setIndex] = useState(
     PRODUCT_STATUE_SET.findIndex((item) => item === productState) || 0
   )
   const onChange = useCallback(
     (index) => {
       replace(`${location.pathname}?productState=${PRODUCT_STATUE_SET[index]}`)
+      setIndex(index)
     },
     [location.pathname, replace]
   )
@@ -51,7 +64,7 @@ export const NftCards: React.FC = () => {
       <Tabs
         colorScheme="black"
         align="space-around"
-        defaultIndex={defaultIndex}
+        defaultIndex={index}
         onChange={onChange}
       >
         <TabList position={'sticky'} top={50} zIndex={99} bg={'white'}>
@@ -61,38 +74,84 @@ export const NftCards: React.FC = () => {
 
         <TabPanels>
           <TabPanel>
-            <InfiniteListNext
-              queryFn={queryFn}
-              queryKey={[Query.Issuers, api, id, productState]}
-              enableQuery={true}
-              emptyElement={<Empty />}
-              noMoreElement={t('common.actions.pull-to-down')}
-              calcDataLength={(data) =>
-                data?.pages.reduce(
-                  (acc, token) => token.token_classes.length + acc,
-                  0
-                ) ?? 0
-              }
-              columnCount={2}
-              renderItems={(group, i) => {
-                return group.token_classes.map((token, j: number) => (
-                  <AspectRatio
-                    ratio={i === 0 && j === 0 ? 1 : 9 / 16}
-                    key={`${i}-${j}`}
-                  >
-                    <Image
-                      src={token.bg_image_url}
-                      width="100%"
-                      height="calc(100% - 5px)"
-                      borderRadius="20px"
-                      resizeScale={300}
-                    />
-                  </AspectRatio>
-                ))
-              }}
-            />
+            {index === 0 ? (
+              <InfiniteListNext
+                enableQuery
+                queryFn={queryFn}
+                queryKey={[Query.Issuers, api, id, productState]}
+                emptyElement={<Empty />}
+                noMoreElement={t('common.actions.pull-to-down')}
+                calcDataLength={(data) =>
+                  data?.pages.reduce(
+                    (acc, token) => token.token_classes.length + acc,
+                    0
+                  ) ?? 0
+                }
+                columnCount={2}
+                renderItems={(group, i) => {
+                  return group.token_classes.map((token, j: number) => (
+                    <AspectRatio
+                      ratio={i === 0 && j === 0 ? 1 : 9 / 12}
+                      key={`${i}-${j}`}
+                    >
+                      <Image
+                        src={token.bg_image_url}
+                        width="100%"
+                        height="calc(100% - 5px)"
+                        rounded="20px"
+                        resizeScale={300}
+                      />
+                    </AspectRatio>
+                  ))
+                }}
+              />
+            ) : null}
           </TabPanel>
-          <TabPanel>2</TabPanel>
+          <TabPanel>
+            {index === 1 ? (
+              <InfiniteListNext
+                enableQuery
+                queryFn={queryFn}
+                queryKey={[Query.Issuers, api, id, productState]}
+                emptyElement={<Empty />}
+                noMoreElement={t('common.actions.pull-to-down')}
+                calcDataLength={(data) =>
+                  data?.pages.reduce(
+                    (acc, token) => token.token_classes.length + acc,
+                    0
+                  ) ?? 0
+                }
+                columnCount={1}
+                renderItems={(group, i) => {
+                  return group.token_classes.map((token, j: number) => (
+                    <Box pb={'25px'}>
+                      <NftImage
+                        type={token.renderer_type}
+                        hasCardBack={token.card_back_content_exist}
+                        src={token.bg_image_url}
+                      />
+                      <Box fontWeight="600" fontSize="16px" mt="14px">
+                        <LineClamp>{token.name}</LineClamp>
+                      </Box>
+                      <Flex justifyContent={'space-between'} mt={'10px'}>
+                        <Box fontWeight="500" fontSize="16px">
+                          ¥{499}
+                        </Box>
+
+                        <Box>
+                          <Like
+                            count={String(token.class_likes)}
+                            liked={token.class_liked}
+                            uuid={token.uuid}
+                          />
+                        </Box>
+                      </Flex>
+                    </Box>
+                  ))
+                }}
+              />
+            ) : null}
+          </TabPanel>
         </TabPanels>
       </Tabs>
     </Box>
