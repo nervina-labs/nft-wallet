9fc2957e2c67cc8a46622d632128f23aebaff879
yuche <i@yuche.me>
2021-10-28 13:12:07 +0800

    refactor: change tx page no data height

--- src/views/Transactions/index.tsx
+++ src/views/Transactions/index.tsx
@@ -45,9 +45,7 @@ const Container = styled(MainContainer)`
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    position: relative;
-    top: 100px;
-    height: 100%;
+    height: 300px;
 
     p {
       margin: 0;
