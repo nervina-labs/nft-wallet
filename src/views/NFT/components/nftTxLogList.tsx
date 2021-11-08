ec9030c6c082e395a4d02c212f3443b327df290b
INS <j893412899@outlook.com>
2021-11-08 20:31:12 +0800

    feat: add copy tx hash

--- src/views/NFT/components/nftTxLogList.tsx
+++ src/views/NFT/components/nftTxLogList.tsx
@@ -10,12 +10,13 @@ import { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import { InfiniteList } from '../../../components/InfiniteList'
 import { useAPI } from '../../../hooks/useAccount'
-import { Query } from '../../../models'
+import { Query, TransactionLog } from '../../../models'
 import { ellipsisString, formatTime } from '../../../utils'
 import TransactionsArrowSvg from '../../../assets/svg/transactions-arrow.svg'
-import { Center, Image } from '@chakra-ui/react'
+import { Center, Image, useClipboard } from '@chakra-ui/react'
 import FallbackAvatarSrc from '../../../assets/svg/fallback.svg'
 import { useHistory } from 'react-router-dom'
+import { useToast } from '../../../hooks/useToast'
 
 const UserWithAddress: React.FC<{
   avatar_url?: string | null
@@ -36,6 +37,7 @@ const UserWithAddress: React.FC<{
     address,
     push,
   ])
+
   return (
     <Grid
       templateColumns="25px calc(100% - 25px - 8px)"
@@ -79,11 +81,47 @@ const UserWithAddress: React.FC<{
   )
 }
 
+const NftTxLog: React.FC<{ log: TransactionLog }> = ({ log }) => {
+  const { t, i18n } = useTranslation('translations')
+  const toast = useToast()
+  const { hasCopied, onCopy } = useClipboard(log.tx_hash)
+  const onCopyTxHash = useCallback(() => {
+    if (hasCopied) return
+    toast(t('info.copied'))
+    onCopy()
+  }, [hasCopied, onCopy])
+
+  return (
+    <Box bg="#f6f6f6" p="20px" rounded="22px" mb="10px">
+      <Box color="#777E90" fontSize="12px" mb="15px">
+        {formatTime(log.on_chain_timestamp, i18n.language)}
+      </Box>
+      <UserWithAddress {...log.sender_info} />
+      <Center w="25px" h="30px" mb="5px">
+        <Image src={TransactionsArrowSvg} alt="TransactionsArrowSvg" />
+      </Center>
+      <UserWithAddress {...log.holder_info} />
+      <Box color="#777E90" fontSize="12px" mt="15px">
+        {t('nft.tx_hash')}:{' '}
+        <Button
+          variant="link"
+          fontSize="12px"
+          colorScheme="primary"
+          fontWeight="300"
+          onClick={onCopyTxHash}
+        >
+          {ellipsisString(log.tx_hash, [10, 10])}
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
 export const NftTxLogsList: React.FC<{
   uuid: string
   isClass?: boolean
 }> = ({ uuid, isClass }) => {
-  const { t, i18n } = useTranslation('translations')
+  const { t } = useTranslation('translations')
   const api = useAPI()
   const queryFn = useCallback(
     async ({ pageParam = 0 }) => {
@@ -104,29 +142,9 @@ export const NftTxLogsList: React.FC<{
         data?.pages.reduce((acc, page) => page.transactions.length + acc, 0) ??
         0
       }
-      renderItems={(items, index) => {
-        return items.transactions.map((item) => (
-          <Box bg="#f6f6f6" p="20px" rounded="22px" mb="10px">
-            <Box color="#777E90" fontSize="12px" mb="15px">
-              {formatTime(item.on_chain_timestamp, i18n.language)}
-            </Box>
-            <UserWithAddress {...item.sender_info} />
-            <Center w="25px" h="30px" mb="5px">
-              <Image src={TransactionsArrowSvg} alt="TransactionsArrowSvg" />
-            </Center>
-            <UserWithAddress {...item.holder_info} />
-            <Box color="#777E90" fontSize="12px" mt="15px">
-              {t('nft.tx_hash')}:{' '}
-              <Button
-                variant="link"
-                fontSize="12px"
-                colorScheme="primary"
-                fontWeight="300"
-              >
-                {ellipsisString(item.tx_hash, [10, 10])}
-              </Button>
-            </Box>
-          </Box>
+      renderItems={(items, i) => {
+        return items.transactions.map((item, j) => (
+          <NftTxLog key={`${i}-${j}`} log={item} />
         ))
       }}
     />
