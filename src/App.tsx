13ce472c94d38b25b93ef58b3051a146f0aadecc
yuche <i@yuche.me>
2021-11-09 11:27:47 +0800

    refactor: upgrade mibao-ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { QueryClientProvider, QueryClient } from 'react-query'
 import { Routers } from './routes'
 import { Provider as JotaiProvider } from 'jotai'
 import { IntryUrl } from './hooks/useWechat'
-import { MibaoProvider } from '@mibao-ui/components'
+import { I18nextProvider } from 'react-i18next'
 import '@mibao-ui/components/mibao-ui.esm.css'
+import i18n from './i18n'
 
 IntryUrl.set(location.href.split('#')[0])
 const App: React.FC = () => {
@@ -13,13 +14,13 @@ const App: React.FC = () => {
   }, [])
 
   return (
-    <MibaoProvider>
+    <I18nextProvider i18n={i18n}>
       <QueryClientProvider client={queryClient}>
         <JotaiProvider>
           <Routers />
         </JotaiProvider>
       </QueryClientProvider>
-    </MibaoProvider>
+    </I18nextProvider>
   )
 }
 
