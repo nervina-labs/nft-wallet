1654b49f835e7a13cc03800ade4f821a49d173b7
INS <j893412899@outlook.com>
2021-11-13 10:13:06 +0800

    feat: update new collection page

--- src/views/Collection/index.tsx
+++ src/views/Collection/index.tsx
@@ -1,10 +1,8 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useQuery } from 'react-query'
 import { Link, Redirect, useParams } from 'react-router-dom'
 import { Query } from '../../models'
-import { TokenClass } from '../../models/class-list'
-import { isSupportWebp } from '../../utils'
 import {
   Appbar as RowAppbar,
   AppbarButton,
@@ -13,105 +11,24 @@ import {
 import { ReactComponent as BackSvg } from '../../assets/svg/back.svg'
 import { MainContainer } from '../../styles'
 import { useAPI } from '../../hooks/useAccount'
-import { Box, Center, Flex, Image } from '@mibao-ui/components'
+import { Box, NFTCard } from '@mibao-ui/components'
 import { InfiniteList } from '../../components/InfiniteList'
-import FALLBACK from '../../assets/svg/fallback.svg'
 import { useHistoryBack } from '../../hooks/useHistoryBack'
 import styled from 'styled-components'
-import { RankTop } from './ranktop'
-import { RoutePath } from '../../routes'
 import { useFirstOpenScrollToTop } from '../../hooks/useFirstOpenScrollToTop'
+import { RoutePath } from '../../routes'
 
-export const Container = styled(MainContainer)`
-  background: linear-gradient(192.04deg, #e5eff5 44.62%, #ffecde 100%);
-  min-height: 100vh;
+const Container = styled(MainContainer)`
   display: flex;
   flex-direction: column;
   position: relative;
-
-  @keyframes run {
-    0% {
-      transform: translateX(0);
-    }
-
-    50% {
-      transform: translateX(50%);
-    }
-
-    100% {
-      transform: translateX(-50%);
-    }
-  }
 `
 
-export const BgAnimation: React.FC = () => {
-  return (
-    <>
-      <Box
-        position="absolute"
-        top="20px"
-        right="20%"
-        w="166px"
-        h="166px"
-        bg="#FFA4E0"
-        filter="blur(50px)"
-        animation="run 5s ease infinite alternate"
-        zIndex={1}
-      />
-      <Box
-        position="absolute"
-        top="10px"
-        left="20%"
-        w="214px"
-        h="214px"
-        bg="#FFEB90"
-        filter="blur(90px)"
-        animation="run 10s ease infinite alternate"
-        zIndex={1}
-      />
-    </>
-  )
-}
-
-export const RankNumber: React.FC<{
-  n: number
-}> = ({ n }) => {
-  return (
-    <Center
-      bg="linear-gradient(192.04deg, #E2E3FF 50.5%, #EADEFF 100%)"
-      m="auto"
-      mr="0"
-      rounded="full"
-      w="20px"
-      h="20px"
-      fontSize="12px"
-    >
-      {n}
-    </Center>
-  )
-}
-
-export const Appbar: React.FC<{ title: string }> = ({ title }) => {
-  const goBack = useHistoryBack()
-  return (
-    <AppbarSticky backdropFilter="blur(10px)">
-      <RowAppbar
-        transparent
-        left={
-          <AppbarButton onClick={goBack}>
-            <BackSvg />
-          </AppbarButton>
-        }
-        title={title}
-      />
-    </AppbarSticky>
-  )
-}
-
 export const Collection: React.FC = () => {
-  const { i18n } = useTranslation('translations')
+  const { t, i18n } = useTranslation('translations')
   const { id } = useParams<{ id: string }>()
   const api = useAPI()
+  const goBack = useHistoryBack()
   const { data, failureCount, error } = useQuery(
     [Query.CollectionDetail, api, id],
     async () => {
@@ -131,7 +48,6 @@ export const Collection: React.FC = () => {
     },
     [api, id]
   )
-  const [topTokenClass, setTopTokenClass] = useState<TokenClass[] | undefined>()
   useFirstOpenScrollToTop()
 
   if (error && failureCount >= 3) {
@@ -140,27 +56,20 @@ export const Collection: React.FC = () => {
 
   return (
     <Container>
-      <Appbar title={data?.locales?.[i18n.language] ?? ''} />
-      <Flex h="200px" justify="center" mb="50px" position="relative" zIndex={2}>
-        {[1, 0, 2].map((v) => (
-          <Box w="105px" key={v}>
-            {topTokenClass?.[v] ? (
-              <RankTop
-                rank={v}
-                bgImageUrl={topTokenClass[v].bg_image_url}
-                name={topTokenClass[v].name}
-                uuid={topTokenClass[v].uuid}
-              />
-            ) : null}
-          </Box>
-        ))}
-      </Flex>
-      <BgAnimation />
+      <AppbarSticky mb="20px">
+        <RowAppbar
+          left={
+            <AppbarButton onClick={goBack}>
+              <BackSvg />
+            </AppbarButton>
+          }
+          title={data?.locales?.[i18n.language] ?? ''}
+        />
+      </AppbarSticky>
       <Box
         px="20px"
         bg="linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #FFFFFF 10%)"
         minH="calc(100vh - 300px)"
-        mt="auto"
       >
         <InfiniteList
           enableQuery
@@ -174,17 +83,34 @@ export const Collection: React.FC = () => {
               0
             ) ?? 0
           }
-          onDataChange={(group) => {
-            setTopTokenClass(group?.pages[0].class_list.slice(0, 3))
-          }}
+          columnCount={2}
+          gap="20px"
           renderItems={(group, i) => {
-            const classList =
-              i === 0
-                ? group.class_list.slice(3, group.class_list.length)
-                : group.class_list
-            return classList.map((token, j: number) => (
+            return group.class_list.map((token, j: number) => (
               <Link to={`/class/${token.uuid}`} key={`${i}-${j}`}>
-                <Flex mb="16px">
+                <NFTCard
+                  src={token.bg_image_url === null ? '' : token.bg_image_url}
+                  type={token.renderer_type}
+                  locale={i18n.language}
+                  title={token.name}
+                  hasCardback={token.card_back_content_exist}
+                  titleProps={{ fontWeight: 'normal' }}
+                  issuerProps={{
+                    name: token.issuer_info?.name ?? '',
+                    src:
+                      token.issuer_info?.avatar_url === null
+                        ? ''
+                        : token.issuer_info?.avatar_url,
+                  }}
+                  limitProps={{
+                    count: token.total,
+                    locale: i18n.language,
+                    limitedText: t('common.limit.limit'),
+                    unlimitedText: t('common.limit.unlimit'),
+                  }}
+                  mb="20px"
+                />
+                {/* <Flex mb="16px">
                   <Image
                     src={token.bg_image_url === null ? '' : token.bg_image_url}
                     width="50px"
@@ -204,8 +130,7 @@ export const Collection: React.FC = () => {
                   >
                     {token.name}
                   </Box>
-                  <RankNumber n={i + j + 4} />
-                </Flex>
+                </Flex> */}
               </Link>
             ))
           }}
