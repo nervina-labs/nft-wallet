df9fc827e1f8100455a6e2523c8eac4a18a8251d
INS <j893412899@outlook.com>
2021-11-16 10:57:32 +0800

    fix: fixed layout aspect ratio

--- src/views/Explore/components/banner.tsx
+++ src/views/Explore/components/banner.tsx
@@ -3,13 +3,20 @@ import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js'
 import { Autoplay, Pagination } from 'swiper'
 import { useAPI } from '../../../hooks/useAccount'
 import { Query } from '../../../models'
-import { Image, Grid, Skeleton, GridItem } from '@mibao-ui/components'
+import {
+  Image,
+  Grid,
+  Skeleton,
+  GridItem,
+  AspectRatio,
+  Box,
+} from '@mibao-ui/components'
 import styled from '@emotion/styled'
 import { useCallback } from 'react'
 
 const Container = styled.div`
   .swiper {
-    height: 200px;
+    height: 100%;
 
     a {
       display: block;
@@ -49,93 +56,92 @@ export const Banner: React.FC = () => {
   return (
     <Container>
       <Skeleton isLoaded={!isLoading} w="full">
-        <Swiper
-          modules={[Autoplay, Pagination]}
-          navigation={false}
-          className="swiper"
-          pagination={{ clickable: true }}
-          loop
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: false,
-          }}
-        >
-          {banners?.map((banner, i) => (
-            <SwiperSlide key={i}>
-              <a
-                onClick={(e) => e.stopPropagation()}
-                target="_blank"
-                style={{
-                  textDecoration: 'none',
-                  backgroundImage: `url(${banner.content})`,
-                }}
-                rel="noopener noreferrer"
-                href={banner.link}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <AspectRatio ratio={385 / 200}>
+          <Swiper
+            modules={[Autoplay, Pagination]}
+            navigation={false}
+            className="swiper"
+            pagination={{ clickable: true }}
+            loop
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false,
+            }}
+          >
+            {banners?.map((banner, i) => (
+              <SwiperSlide key={i}>
+                <a
+                  onClick={(e) => e.stopPropagation()}
+                  target="_blank"
+                  style={{
+                    textDecoration: 'none',
+                    backgroundImage: `url(${banner.content})`,
+                  }}
+                  rel="noopener noreferrer"
+                  href={banner.link}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </AspectRatio>
       </Skeleton>
 
-      <Grid
-        templateColumns="repeat(2, 1fr)"
-        h="190px"
-        w="full"
-        py="10px"
-        px="20px"
-        gap="10px"
-      >
-        {data?.['Notification::News']?.[0] ? (
-          <GridItem
-            rowSpan={2}
-            colSpan={1}
-            overflow="hidden"
-            rounded="22px"
-            roundedBottomRight="0"
-            onClick={gotoLink(data['Notification::News'][0].link)}
-          >
-            <Image
-              src={data['Notification::News'][0].content}
-              w="full"
-              h="full"
-            />
-          </GridItem>
-        ) : null}
+      <AspectRatio ratio={336 / 148}>
+        <Box py="10px" px="20px">
+          <Grid templateColumns="repeat(2, 1fr)" w="full" h="full" gap="10px">
+            {data?.['Notification::News']?.[0] ? (
+              <GridItem
+                rowSpan={2}
+                colSpan={1}
+                overflow="hidden"
+                rounded="22px"
+                roundedBottomRight="0"
+                onClick={gotoLink(data['Notification::News'][0].link)}
+              >
+                <Image
+                  src={data['Notification::News'][0].content}
+                  w="full"
+                  h="full"
+                />
+              </GridItem>
+            ) : null}
 
-        {data?.['Notification::Interview']?.[0] ? (
-          <GridItem
-            rowSpan={1}
-            colSpan={1}
-            overflow="hidden"
-            rounded="22px"
-            roundedTopLeft="0"
-            onClick={gotoLink(data['Notification::Interview'][0].link)}
-          >
-            <Image
-              src={data['Notification::Interview'][0].content}
-              w="full"
-              h="full"
-            />
-          </GridItem>
-        ) : null}
+            {data?.['Notification::Interview']?.[0] ? (
+              <GridItem
+                rowSpan={1}
+                colSpan={1}
+                overflow="hidden"
+                rounded="22px"
+                roundedTopLeft="0"
+                onClick={gotoLink(data['Notification::Interview'][0].link)}
+              >
+                <Image
+                  src={data['Notification::Interview'][0].content}
+                  w="full"
+                  h="full"
+                />
+              </GridItem>
+            ) : null}
 
-        {data?.['Notification::Activity']?.[0] ? (
-          <GridItem
-            rowSpan={1}
-            colSpan={1}
-            overflow="hidden"
-            rounded="22px"
-            roundedBottomLeft="0"
-            onClick={gotoLink(data['Notification::Activity'][0]?.link)}
-          >
-            <Image
-              src={data['Notification::Activity'][0].content}
-              w="full"
-              h="full"
-            />
-          </GridItem>
-        ) : null}
-      </Grid>
+            {data?.['Notification::Activity']?.[0] ? (
+              <GridItem
+                rowSpan={1}
+                colSpan={1}
+                overflow="hidden"
+                rounded="22px"
+                roundedBottomLeft="0"
+                onClick={gotoLink(data['Notification::Activity'][0]?.link)}
+              >
+                <Image
+                  src={data['Notification::Activity'][0].content}
+                  w="full"
+                  h="full"
+                />
+              </GridItem>
+            ) : null}
+          </Grid>
+        </Box>
+      </AspectRatio>
     </Container>
   )
 }
