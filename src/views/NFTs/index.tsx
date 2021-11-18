891471bf8d7882220f61f7647730784ba3456074
yuche <i@yuche.me>
2021-11-18 14:13:49 +0800

    feat: wechat share

--- src/views/NFTs/index.tsx
+++ src/views/NFTs/index.tsx
@@ -2,7 +2,7 @@
 import React, { useMemo } from 'react'
 import { useQuery } from 'react-query'
 import { Query } from '../../models'
-import { Redirect, useParams } from 'react-router'
+import { Redirect, useParams, useRouteMatch } from 'react-router'
 import { RoutePath } from '../../routes'
 import { HiddenBar, HiddenBarFill } from '../../components/HiddenBar'
 import { useScrollRestoration } from '../../hooks/useScrollRestoration'
@@ -12,6 +12,8 @@ import { Info } from './info'
 import { useAccount, useAccountStatus, useAPI } from '../../hooks/useAccount'
 import { Appbar } from './components/appbar'
 import { NftList } from './components/nftList'
+import { useWechatShare } from '../../hooks/useWechat'
+import { useTranslation } from 'react-i18next'
 
 export const NFTs: React.FC = () => {
   const params = useParams<{ address?: string }>()
@@ -24,11 +26,23 @@ export const NFTs: React.FC = () => {
   )
   const isHolder = useMemo(() => Boolean(params.address), [params.address])
   useScrollRestoration()
+  const wechatShare = useWechatShare()
+  const [t] = useTranslation('translations')
+  const matchHome = useRouteMatch(RoutePath.NFTs)
   const { data: user, isLoading: isUserLoading } = useQuery(
     [Query.Profile, address, api],
     async () => await api.getProfile(address),
     {
       enabled: !!address,
+      onSuccess: (d) => {
+        wechatShare({
+          title: t('common.share.wx.collector.title', {
+            name: d.nickname || t('holder.title'),
+          }),
+          desc: t(`common.share.wx.${matchHome ? 'home' : 'collector'}.desc`),
+          link: `${location.origin}${RoutePath.Holder}/${address}`,
+        })
+      },
     }
   )
 
