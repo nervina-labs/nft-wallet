1cd5f9df90e9f1959ffae0e9c01f718c6e468d27
INS <j893412899@outlook.com>
2021-12-01 17:42:41 +0800

    merge `develop` to `staging` (#341) (#342)
    * chore: release tag by date
    
    * hotfix: add wechat with android 3d tips (#340)
    
    * chore: hard code wxid
    
    * chore: change desc
    
    * feat: add wechat with android tips
    
    * feat: add 3d tips
    
    * fix: remove `console`
    
    Co-authored-by: Yu Che <i@yuche.me>
    
    Co-authored-by: yuche <i@yuche.me>
    
    Co-authored-by: yuche <i@yuche.me>

--- src/components/ThreeDPreview/index.tsx
+++ src/components/ThreeDPreview/index.tsx
@@ -1,8 +1,13 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Center, Flex } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import '@google/model-viewer'
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { ReactComponent as ThreeDLoadingSvg } from '../../assets/svg/loading.svg'
+import { ReactComponent as ThreeDTipsArrowSvg } from '../../assets/svg/three-d-tips-arrow.svg'
+import { ReactComponent as ThreeDTipsIconSvg } from '../../assets/svg/three-d-tips-icon.svg'
+import { ReactComponent as ThreeDTipsCloseSvg } from '../../assets/svg/three-d-tips-close.svg'
+import { IS_ANDROID, IS_WEXIN } from '../../constants'
 
 interface ThreeDPreviewProps {
   src: string
@@ -19,6 +24,16 @@ const ThreeDPreviewContainer = styled(Box)`
   width: 100%;
   height: 100%;
   position: relative;
+
+  .tips-icon {
+    margin-right: 6px;
+  }
+
+  .tips-arrow {
+    position: absolute;
+    top: 0;
+    right: -60px;
+  }
 
   .three-d-loading {
     width: 100px;
@@ -92,6 +107,7 @@ const ThreeDPreviewWithLoading: React.FC<{
   onError?: () => void
   loader?: string
 }> = ({ src, onError, loader }) => {
+  const { t } = useTranslation('translations')
   const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     setIsLoading(Boolean(src))
@@ -101,8 +117,45 @@ const ThreeDPreviewWithLoading: React.FC<{
     setIsLoading(false)
   }, [])
 
+  const [isClosedTips, setIsClosedTips] = useState(false)
+
   return (
     <ThreeDPreviewContainer>
+      {IS_ANDROID && IS_WEXIN && !isClosedTips ? (
+        <Box
+          position="absolute"
+          top="8px"
+          left="calc(50% - 20px)"
+          w="245px"
+          bg="white"
+          fontSize="13px"
+          rounded="8px"
+          py="10px"
+          px="20px"
+          transform="translateX(-60%)"
+          zIndex={1}
+          onClick={() => setIsClosedTips(true)}
+        >
+          <Flex fontSize="14px" fontWeight="bold" w="full" alignItems="center">
+            <ThreeDTipsIconSvg className="tips-icon" />
+            {t('common.3d.tips-title')}
+          </Flex>
+          {t('common.3d.tips-desc')}
+          <ThreeDTipsArrowSvg className="tips-arrow" />
+          <Center
+            position="absolute"
+            top="10px"
+            right="10px"
+            bg="#F5F5F5"
+            rounded="full"
+            w="15px"
+            h="15px"
+          >
+            <ThreeDTipsCloseSvg />
+          </Center>
+        </Box>
+      ) : null}
+
       {src ? (
         <ThreeDPreview src={src} onLoad={onLoad} onError={onError} />
       ) : null}
