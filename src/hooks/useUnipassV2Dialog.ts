51061ffa6e3bc60456d9695035f0ecdffa026d9d
yuche <i@yuche.me>
2022-02-24 15:00:46 +0800

    fix: double warning if v2 is logined

--- src/hooks/useUnipassV2Dialog.ts
+++ src/hooks/useUnipassV2Dialog.ts
@@ -6,8 +6,8 @@ export const useUnipassV2Dialog = () => {
   const [t] = useTranslation('translations')
   const confirmDialog = useConfirmDialog()
   const logout = useLogout()
-  return (): void => {
-    confirmDialog({
+  return async (): Promise<void> => {
+    return await confirmDialog({
       type: 'error',
       title: t('common.unipass-v2-error'),
       okText: t('common.relogin'),
