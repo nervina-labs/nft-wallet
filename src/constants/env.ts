f7a8fdb621eb4190683eeef120cd4a3f4c2cc994
yuche <i@yuche.me>
2022-03-18 15:55:27 +0800

    chore: update url

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -80,7 +80,7 @@ if (IS_MAINNET) {
   Config.setChainType('mainnet')
 } else {
   Config.setChainType('testnet')
-  Config.setFlashsignerURL('https://flashsigner-5yk1wxcko-nervina.vercel.app')
+  Config.setFlashsignerURL('https://flashsigner-3fwvzrmn7-nervina.vercel.app/')
 }
 
 export const RED_ENVELOP_APP_URL = IS_MAINNET
