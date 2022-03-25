6030f9179dbaaeda2a3435075c3d6541f3728ca0
yuche <i@yuche.me>
2022-03-25 15:26:02 +0800

    chore: bump new version

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -26,7 +26,8 @@ clientsClaim()
 // This variable must be present somewhere in your service worker file,
 // even if you decide not to use precaching. See https://cra.link/PWA
 precacheAndRoute(self.__WB_MANIFEST)
-console.log('Verion: v2.2.0')
+const log = console.log
+log('Verion: v2.2.1')
 
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
